f819a2013c4b95224526ac9a995e85706607d2f5
unknown <99067235@mydavinci.nl>
2021-09-07 14:11:45 +0200

    Antwoord op aanroepen van hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+# Naam:Jurrian Schouten
+# Studentennummer:99067235
+
+print("Hallo wereld van...")
+print("")
+print("pppp    y     y   ttttttt   h   h    oooo   nnnn")
+print("p   p    y   y       t      h   h   o    o  n   n")
+print("p   p     y y        t      h   h   o    o  n   n")
+print("pppp       y         t      hhhhh   o    o  n   n")
+print("p          y         t      h   h   o    o  n   n")
+print("p          y         t      h   h   o    o  n   n")
+print("p          y         t      h   h    oooo   n   n")
